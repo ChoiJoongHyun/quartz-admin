4d1a1d0b21e7d1fd39946731f4af4f1773cf3665
ChoiJoongHyun <joonghyun0117@gmail.com>
2018-04-26 18:45:29 +0900

    init project

---
+++ src/main/java/com/dotori/quartzadmin/QuartzAdminApplication.java
@@ -0,0 +1,12 @@
+package com.dotori.quartzadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzAdminApplication.class, args);
+    }
+}
---
+++ src/test/java/com/dotori/quartzadmin/QuartzAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dotori.quartzadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuartzAdminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
