8b2f2e9448237b280a3d417b5b2717bba7eb0a77
ChoiJoongHyun <joonghyun0117@gmail.com>
2018-07-26 18:46:39 +0900

    트리거 리스트 구현

--- src/front/pages/Trigger.vue
+++ src/front/pages/Trigger.vue
@@ -1,12 +1,48 @@
 <template>
-    <div>
-        Trigger Page
+    <div class="table-responsive">
+
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>jobName</th>
+                <th>jobGroup</th>
+                <th>nextFireTime</th>
+                <th>triggerState</th>
+                <th>triggerType</th>
+            </tr>
+            </thead>
+            <tbody>
+
+            <tr v-for="item in list" v-bind:key="item.jobName" >
+                <td>{{item.jobName}}</td>
+                <td>{{item.jobGroup}}</td>
+                <td>{{item.nextFireTime}}</td>
+                <td>{{item.triggerState}}</td>
+                <td>{{item.triggerType}}</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default {
-        name: "Trigger"
+        name: "Trigger",
+        data() {
+            return {
+                list: []
+            }
+        },
+        created: function () {
+            axios.get('http://localhost:3000/api/triggers').then(res => {
+                this.list = res.data;
+            }).catch(error => {
+                //TODO error
+                console.log(error);
+            })
+        }
     }
 </script>
 
